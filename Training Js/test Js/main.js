b98f9aa503ff89554d3722b0a3ff7afc4e73f9b4
TranThanh2k1 <thanhttph11081@gmail.com>
2021-03-26 11:59:25 +0700

    update

--- Training Js/test Js/main.js
+++ Training Js/test Js/main.js
@@ -1,25 +1,33 @@
 var $ = document.querySelector.bind(document);
 
 // var ktra = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{5,10}$";
+var kt = /[a-zA-Z]/
 
 $('#form-login').addEventListener('submit', function (e) {
   e.preventDefault(); // chặn sự kiện load trang mặc định của website
   if ($('#username').value == "") {
-    alert("Mời nhập tên đăng nhập");
-    $('#username').focus();
-    return false;
+    // alert("Mời nhập tên đăng nhập");
+    $('#username').style.border = '1px solid #FF0000';
+    // return false;
   }
+
+  if ($('#username').style.border = '1px solid #FF0000') {
+    $('#username').style.border = '1px soild #000000'
+  }
+  // $('#username').addEventListener('change', function () {
+  //   $('#username').style.display = "none";
+  // })
 
   if ($('#password').value.length == "") {
     alert("Mời nhập mật khẩu");
     $('#password').focus();
     return false;
   }
-  // if ($('#password').value.length != ktra) {
-  //   alert("oke");
-  //   $('#password').focus();
-  //   return false;
-  // }
+  if ($('#password').value.length = kt) {
+    alert("oke");
+    $('#password').focus();
+    return false;
+  }
   if ($('#password').value.length < 5 || $('#password').value.length > 10) {
     alert("Mật khẩu phải lớn hơn 5 kí tự và nhỏ hơn 10 kí tự");
     $('#password').focus();
@@ -30,7 +38,9 @@ $('#form-login').addEventListener('submit', function (e) {
 })
 
 
-
+/*
+console.log(window.location.hash);
+*/
 
 // Code cho anh 1 trang đăng nhập.Có 2 ô input
 //   + Username.
