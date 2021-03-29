c378147dc2594955aef0499582dccdfe9d6a0bf4
TranThanh2k1 <thanhttph11081@gmail.com>
2021-03-29 18:56:01 +0700

    ham xu li chuoi

---
+++ JavaScript nâng cao/ham_xu_li_chuoi_mang/index.js
@@ -0,0 +1,45 @@
+var mang = ["Trần Tiến Thành", "Trần Hữu Mạnh"];
+var chuoi = 'Trần Tiến Thành';
+// var arr = [1, 2, 3, 4];
+// Hàm xử lí chuỗi và mảng
+// 1. toString(), join(): chuyển 1 mảng về dạng chuỗi kí tự ngắn cách nhau bằng dấu phẩy
+// console.log(mang.toString(""));
+
+// 2. push(): thêm phần tử vào cuối mảng
+// mang.push("Nguyễn Văn A");
+// console.log(mang);
+
+// 3. pop(): loại bỏ phần tử cuối cùng của mảng
+// mang.pop();
+// console.log(mang);
+
+// 4. unshift(): thêm 1 phần tử vào đầu mảng
+// mang.unshift("Nguyễn Văn A");
+// console.log(mang);
+
+// 5. shift(): loại bỏ phần tử đầu tiên của mảng
+// mang.shift();
+// console.log(mang);
+
+// 6. slice(start, stop): dùng để cắt chuỗi từ phần tử start đến stop(start ko đc lơn hơn stop,
+// start là số âm thì cắt chuỗi tính từ cuối đến đầu)
+// var chuoi1 = chuoi.slice(2, 6);
+// console.log(chuoi1);
+
+// 7. substring(start, stop): giống slice(nếu start > stop thì stop=start)
+
+// 8. substr(start, length): cắt chuỗi tính từ vị trí index start và số lượng kí tự length
+// var chuoi1 = chuoi.substr(3, 4);
+// console.log(chuoi1);
+
+// 9. toUpperCase(): trả về 1 chuỗi chữ hoa từ chuỗi gốc
+// var chuoi1 = chuoi.toUpperCase();
+// console.log(chuoi1);
+
+// 10. toLowerCase(): trả về 1 chuỗi chữ thường từ chuỗi gốc
+// var chuoi1 = chuoi.toLowerCase();
+// console.log(chuoi1);
+
+// 11. trim(): trả về 1 chuỗi đã loại bỏ koangr trắng
+
+// 12. replace(): tìm và thay thế giá trị đó(chỉ tìm và thay thế gtri đầu) 
