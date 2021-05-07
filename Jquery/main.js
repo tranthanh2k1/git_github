4d32c17d2e90341b313e2a6fb1934337d8e1a5c6
Thành Trần <thanhttph11081@gmail.com>
2021-05-07 17:32:13 +0700

    he

---
+++ Jquery/main.js
@@ -0,0 +1,25 @@
+// Sự kiện trong Jquery
+// $(document).ready(): xảy ra đầu tiên khi DOM đc load xong
+// $(window).load(): xảy ra khi tất cả các DOM và toàn bộ nội dung của trang đc tải xong
+// Cú pháp: $(selector).action()
+
+// Seclector: $('tên của phần tử - viết theo kiểu css)
+// Event trong Jquery:
+// Mouse Events(Sự kiện liên quan đến chuột): click, dbclick, mouseenter, mouseleave
+// Keyboard Events(Sự kiện liên quan đến bàn phím): keypress, keydown, keyup
+// Form Events(Sự kiện liên quan đến form): submit, change focus, blur
+// Document/Windown Events: load, resize, scroll, unload
+
+// Hiệu ứng trong Jquery
+// hide(): ẩn
+// show(): hiện
+// toggle(): ẩn/hiện
+// Jquery Fade: làm mờ
+//  + fadeIn(): làm mờ 1 phần tử ấn
+//  + fadeOut(): làm mờ phần tử hiển thị
+//  + fadeToggle(): chuyển đổi giữa fadeIn() và fadeOut()
+// Jquery Slide: trượt các phần tử lên xuống
+//  + slideDown(): trượt xuống 1 phần tử
+//  + slideUp(): trượt lên 1 phần tử
+//  + slideToggle(): chuyển đổi giữa slideDown() và slideUp()
+// Jquery Animate: tạo hoạt ảnh tùy chỉnh
