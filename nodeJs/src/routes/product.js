4f668e25e3a337dae6f95f6515104f0ce7d57749
Thành Trần <thanhttph11081@gmail.com>
2021-06-25 16:25:41 +0700

    update 25-6

--- nodeJs/src/routes/product.js
+++ nodeJs/src/routes/product.js
@@ -1,8 +1,19 @@
 const express = require('express');
-const { create } = require('../controllers/product');
+const { create, list, remove, update, searchProduct, productID } = require('../controllers/product');
 
 const router = express.Router();
 
+// Thêm sản phẩm
 router.post('/product', create);
+// Danh sách sản phẩm
+router.get('/products', list);
+router.put('/product/:id', update);
+// Xóa sản phẩm
+router.delete('/product/:id', remove);
+
+// tìm kiếm sản phẩm theo tên
+router.get('/product/search', searchProduct);
+
+router.param('id', productID);
 
 module.exports = router;
